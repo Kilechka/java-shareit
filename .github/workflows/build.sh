1c90148f1a7c49a2f321910fd681217114338b43
Liliya <Kilechkaheh@yandex.ru>
2024-08-02 19:53:36 +0300

    .

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+echo "Installing Docker Compose..."
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
+
 cp -rf ./tests/checkstyle.xml ./checkstyle.xml
 cp -rf ./tests/suppressions.xml ./suppressions.xml
 mvn enforcer:enforce -Drules=requireProfileIdsExist -P check --no-transfer-progress
